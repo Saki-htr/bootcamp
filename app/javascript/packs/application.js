bbbed7af525780e1b38b8cc5630c7a97cc25dd0c
Nabegon <watami03@gmail.com>
2022-08-22 22:56:39 +0200

    コンフリクト解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -75,19 +75,10 @@ import AdminCompanies from '../components/admin_companies.vue'
 import Announcements from '../components/announcements.vue'
 import Books from '../components/books.vue'
 import Pages from '../components/pages.vue'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Questions from '../components/questions.vue'
 import WorriedUsers from '../components/worried-users.vue'
 import Reports from '../components/reports.vue'
-=======
-=======
-import Questions from '../components/questions.vue'
-import WorriedUsers from '../components/worried-users.vue'
-import Reports from '../components/reports.vue'
->>>>>>> コンフリクト解消
 import Footprints from '../components/footprints.vue'
->>>>>>> 決め打ちのデータでfootprintコンポーネントを作成
 
 const mounter = new VueMounter()
 mounter.addComponent(Hello)
@@ -95,17 +86,8 @@ mounter.addComponent(AdminCompanies)
 mounter.addComponent(Announcements)
 mounter.addComponent(Books)
 mounter.addComponent(Pages)
-<<<<<<< HEAD
-<<<<<<< HEAD
 mounter.addComponent(Questions)
 mounter.addComponent(WorriedUsers)
 mounter.addComponent(Reports)
-=======
-=======
-mounter.addComponent(Questions)
-mounter.addComponent(WorriedUsers)
-mounter.addComponent(Reports)
->>>>>>> コンフリクト解消
 mounter.addComponent(Footprints)
->>>>>>> 決め打ちのデータでfootprintコンポーネントを作成
 mounter.mount()
